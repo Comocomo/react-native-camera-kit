591e23af2f87493e43712efe8a77824d0509108d
Gal <gal@nutrinohealth.com>
2018-11-22 17:40:55 +0200

    changed to match master

--- src/CameraScreen/CameraKitCameraScreen.ios.js
+++ src/CameraScreen/CameraKitCameraScreen.ios.js
@@ -7,7 +7,6 @@ export default class CameraScreen extends CameraScreenBase {
 
   
   render() {
-      console.log("[react-native-camera-kit]: render in CameraScreen")
     return (
       <View style={{ flex: 1, backgroundColor: 'black' }} {...this.props}>
         {this.renderTopButtons()}
